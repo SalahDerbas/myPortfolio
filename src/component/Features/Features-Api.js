3f3cdc6a51178944c76a2214730e97e9b3b12c84
Sdrbas <sdrbas@tandratech.com>
2022-07-06 17:17:33 +0300

    image features

--- src/component/Features/Features-Api.js
+++ src/component/Features/Features-Api.js
@@ -1,19 +1,19 @@
 const data = [
   {
     id: 1,
-    image: "https://www.scd.stfc.ac.uk/Gallery/SEG_wordcloud_2.png",
+    image: "./features/software.png",
     title: "Software Engineering",
     desc: "I handle the design, development, testing and maintenance of software applications and I addition to applying engineering principles and knowledge of programming languages to build software solutions for end users.",
   },
   {
     id: 2,
-    image: "https://devwebx.com/wp-content/uploads/2018/07/Why_Laravel.jpg",
+    image: "./features/backend.jpg",
     title: "Backend Developer",
     desc: " I am building code that allows a database and an application to communicate with each other. I take care of the backend development and maintenance of the backend of a website, including databases, servers, and applications",
   },
   {
     id: 3,
-    image: "https://wallpaperaccess.com/full/4635737.jpg ",
+    image: "./features/frontend.webp",
     title: "Frontend Developer",
     desc: "I build front end websites and applications using web languages like HTML, CSS ,React-js , Bootstrap , TypeScript ,Ajax and JavaScript allowing users to access and interact with the site or application.",
   },
