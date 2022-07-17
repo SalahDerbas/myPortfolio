48ad04639098eb64ee4f1dbe9861186a9ce72871
SalahDerbas <salahderbas123@gmail.com>
2022-07-18 01:41:22 +0300

    change all photos

--- src/component/Features/Features-Api.js
+++ src/component/Features/Features-Api.js
@@ -1,19 +1,20 @@
 const data = [
   {
     id: 1,
-    image: "./features/software.png",
-    title: "Software Engineering",
-    desc: "I handle the design, development, testing and maintenance of software applications and I addition to applying engineering principles and knowledge of programming languages to build software solutions for end users.",
-  },
-  {
-    id: 2,
-    image: "./features/backend.jpg",
+    image: "./features/backend.png",
     title: "Backend Developer",
     desc: "I take care of the backend development and maintenance of the backend of a website, including databases, servers, and applications",
   },
   {
+    id: 2,
+    image: "./features/software.jpg",
+    title: "Software Engineering",
+    desc: "I handle the design, development, testing and maintenance of software applications and I addition to applying engineering principles and knowledge of programming languages to build software solutions for end users.",
+  },
+  
+  {
     id: 3,
-    image: "./features/frontend.webp",
+    image: "./features/frontend.png",
     title: "Frontend Developer",
     desc: "I build front end websites and applications using web languages like HTML, CSS ,React-js , Bootstrap , TypeScript ,Ajax and JavaScript allowing users to access and interact with the site or application.",
   },
